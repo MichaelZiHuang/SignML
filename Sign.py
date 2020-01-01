45a5a1fc6fc2591f36e46354a3940e3cd1e995ed
Michael Huang <sleep3030@gmail.com>
2019-12-31 21:11:03 -0800

    Additional generation

--- Sign.py
+++ Sign.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential, load_model
-from keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.optimizers import SGD
@@ -16,12 +16,12 @@ import matplotlib.pyplot as plt
 
 
 
-trainer = read_csv("C:/Users/Huang/Documents/GitHub/SignML/sign_mnist_train/sign_mnist_train.csv")
+trainer = read_csv("C:/Users/Michael Huang/Documents/GitHub/SignML/sign_mnist_train/sign_mnist_train.csv")
 labels = trainer["label"].values
 trainer = trainer.drop(["label"], axis=1) #
 
 
-tester = read_csv("C:/Users/Huang/Documents/GitHub/SignML/sign_mnist_test/sign_mnist_test.csv")
+tester = read_csv("C:/Users/Michael Huang/Documents/GitHub/SignML/sign_mnist_test/sign_mnist_test.csv")
 testlabels = tester["label"].values
 tester = tester.drop(["label"], axis=1)
 
@@ -60,10 +60,10 @@ def defineModel():
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(BatchNormalization())
 
-    #model.add(Conv2D(128, (3, 3), activation='relu',padding='same'))
-    #model.add(Dropout(0.2))
-    #model.add(MaxPooling2D(pool_size=(2, 2)))
-    #model.add(BatchNormalization())
+    model.add(Conv2D(128, (3, 3), activation='relu',padding='same'))
+    model.add(Dropout(0.2))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(BatchNormalization())
 
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
@@ -71,8 +71,8 @@ def defineModel():
     model.add(Dense(y_train.shape[1], activation='softmax'))
     
 
-    opt = SGD(lr=0.0008, momentum=0.9)
-    model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["categorical_accuracy"])
+    #opt = SGD(lr=0.0008, momentum=0.9)
+    model.compile(optimizer='adam', loss="categorical_crossentropy", metrics=["categorical_accuracy"])
     return model
 
 
@@ -95,11 +95,14 @@ def summarize_diagnostics(history):
 
 def testModel():
     model = load_model("my_model.hl5")
-    #folder = "C:/Users/Huang/Documents/GitHub/SignML/"
-    
-    print(model.evaluate(x_test, y_test))
+    #folder = "C:/Users/Michael Huang/Documents/GitHub/SignML/"
+    datagen = ImageDataGenerator()
+    datagen.fit(x_test)
+    #_, acc = model.evaluate_generator(datagen.flow(x_test, y_test), steps=(len(x_test)), verbose=1)
+    #print('> %.3f' % (acc * 100.0))
+
     #pic = input("Give me the extension:")
-    img = load_img("C:/Users/Huang/Documents/GitHub/SignML/a.jpg", color_mode="grayscale", target_size=(28, 28))
+    img = load_img("C:/Users/Michael Huang/Documents/GitHub/SignML/Ctrue.png", color_mode="grayscale", target_size=(28, 28))
     img = img_to_array(img)
     img = img.flatten()
     img = np.reshape(img, (-1, 28, 28, 1))
@@ -131,20 +134,44 @@ if __name__ == "__main__":
     x_test = x_test/255.0
     x_test = np.reshape(x_test, (x_test.shape[0], 28, 28, 1))
 
+    
     #testModel()
-    choice = int(input("Load or Define? (0, 1)"))
+    choice = 0
     if(choice == 0):
         #model = load_model("my_model.hl5")
         testModel()
     else:
-        model = defineModel()
-        history =  model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs=30, verbose=1, batch_size=128)
-        summarize_diagnostics(history)
+       # model = defineModel()
+       # history =  model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs=30, verbose=1, batch_size=128)
+       # summarize_diagnostics(history)
+        x_train2 = np.array(x_train, copy=True)
+        y_train2 = np.array(y_train, copy=True)
 
-        testStuff, testlabels = preProcessing(tester, testlabels)
-        testStuff = testStuff.reshape(testStuff.shape[0], 28, 28, 1)
-        y_pred = model.predict(testStuff).round()
-        print(accuracy_score(testlabels, y_pred))
-    #score = accuracy_score()
+        datagen = ImageDataGenerator(featurewise_center=False, 
+                                    samplewise_center=False,
+                                     featurewise_std_normalization=False, 
+                                     samplewise_std_normalization=False,
+                                     width_shift_range=0.2,
+                                     height_shift_range=0.2,
+                                     horizontal_flip=True,
+                                     vertical_flip=True, 
+                                     rotation_range=10)
+        datagen.fit(x_train)
+
+        datagenTest = ImageDataGenerator()
+        datagen.fit(x_test)
+
+        new_x = np.concatenate((x_train, x_train2), axis=0)
+        new_y = np.concatenate((y_train, y_train2), axis=0)
+
+        model = defineModel()
+        history = model.fit_generator(datagen.flow(new_x, new_y, batch_size=32), steps_per_epoch=len(x_train)/32, validation_data=(x_test, y_test), verbose=1, epochs=20)
         model.save("my_model.hl5")
+        #testStuff, testlabels = preProcessing(tester, testlabels)
+        #testStuff = testStuff.reshape(testStuff.shape[0], 28, 28, 1)
+        #y_pred = model.predict(testStuff).round()
+        #print(accuracy_score(testlabels, y_pred))
+        #score = accuracy_score()
+        #model.save("my_model.hl5")
+
 
