d99213e61c38946f527b8748f199205f5d2cb75f
MichaelZiHuang <sleep3030@gmail.com>
2019-12-31 16:43:00 -0800

    Adding some regulation

--- Sign.py
+++ Sign.py
@@ -3,6 +3,9 @@ from keras.preprocessing.image import load_img, img_to_array
 from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.optimizers import SGD
+from keras.regularizers import l2
+
+import sys
 
 import numpy as np
 from pandas import read_csv
@@ -13,12 +16,12 @@ import matplotlib.pyplot as plt
 
 
 
-trainer = read_csv("C:/Users/Michael Huang/Documents/GitHub/SignML/sign_mnist_train/sign_mnist_train.csv")
+trainer = read_csv("C:/Users/Huang/Documents/GitHub/SignML/sign_mnist_train/sign_mnist_train.csv")
 labels = trainer["label"].values
 trainer = trainer.drop(["label"], axis=1) #
 
 
-tester = read_csv("C:/Users/Michael Huang/Documents/GitHub/SignML/sign_mnist_test/sign_mnist_test.csv")
+tester = read_csv("C:/Users/Huang/Documents/GitHub/SignML/sign_mnist_test/sign_mnist_test.csv")
 testlabels = tester["label"].values
 tester = tester.drop(["label"], axis=1)
 
@@ -46,28 +49,29 @@ def preProcessing(stuff, classes):
 def defineModel():
     model = Sequential()
 
-    model.add(Conv2D(64, (3, 3), input_shape=(x_test.shape[1:]), activation='relu', padding='same'))
-    model.add(Dropout(0.2))
+    model.add(Conv2D(32, (3, 3), input_shape=(x_test.shape[1:]), activation='relu', padding='same', activity_regularizer=l2(0.001)))
+    #model.add(Activation('relu'))
+    #model.add(Dropout(0.2))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(BatchNormalization())
 
-    model.add(Conv2D(64, (3, 3), activation='relu',padding='same'))
-    model.add(Dropout(0.2))
+    model.add(Conv2D(64, (3, 3), activation='relu',padding='same' ))
+    model.add(Dropout(0.5))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    #model.add(BatchNormalization())
+    model.add(BatchNormalization())
 
-   # model.add(Conv2D(64, (3, 3), activation='relu',padding='same'))
-   # model.add(Dropout(0.2))
-   # model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Conv2D(128, (3, 3), activation='relu',padding='same'))
+    #model.add(Dropout(0.2))
+    #model.add(MaxPooling2D(pool_size=(2, 2)))
     #model.add(BatchNormalization())
 
     model.add(Flatten())
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.2))
-
     model.add(Dense(y_train.shape[1], activation='softmax'))
-    model.add(Dropout(0.5))
-    opt = SGD(lr=0.001, momentum=0.9)
+    
+
+    opt = SGD(lr=0.0008, momentum=0.9)
     model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["categorical_accuracy"])
     return model
 
@@ -91,23 +95,25 @@ def summarize_diagnostics(history):
 
 def testModel():
     model = load_model("my_model.hl5")
-    #folder = "C:/Users/Michael Huang/Documents/GitHub/SignML/"
+    #folder = "C:/Users/Huang/Documents/GitHub/SignML/"
+    
+    print(model.evaluate(x_test, y_test))
     #pic = input("Give me the extension:")
-    img = load_img("C:/Users/Michael Huang/Documents/GitHub/SignML/TestC.jpg", color_mode="grayscale", target_size=(28, 28))
+    img = load_img("C:/Users/Huang/Documents/GitHub/SignML/a.jpg", color_mode="grayscale", target_size=(28, 28))
     img = img_to_array(img)
-    #img = img.flatten()
-    #img = np.reshape(img, (-1, 28, 28, 1))
+    img = img.flatten()
+    img = np.reshape(img, (-1, 28, 28, 1))
     #img = np.reshape(img, (28, 28))
     img = img/255.0
     #plt.imshow(img)
     #plt.show()
 
-    #test = model.predict_classes(img)
-    #print(test)
+    test = model.predict_classes(img)
+    print(test)
     #for i in range(3):
-    #test_test = model.predict_proba(img)
-    #test_test = "%.2f" % (test_test[test]*100)
-    #print(test_test)
+    test_test = model.predict_proba(img)[0]
+    test_test = "%.2f" % (test_test[test]*100)
+    print(test_test)
     
 
 
@@ -125,17 +131,20 @@ if __name__ == "__main__":
     x_test = x_test/255.0
     x_test = np.reshape(x_test, (x_test.shape[0], 28, 28, 1))
 
+    #testModel()
+    choice = int(input("Load or Define? (0, 1)"))
+    if(choice == 0):
+        #model = load_model("my_model.hl5")
+        testModel()
+    else:
+        model = defineModel()
+        history =  model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs=30, verbose=1, batch_size=128)
+        summarize_diagnostics(history)
 
-   
-    model = defineModel()
-    
-    history =  model.fit(x_train, y_train, validation_data = (x_test, y_test), epochs=40, verbose=1, batch_size=128)
-    summarize_diagnostics(history)
-
-   # testStuff, testlabels = preProcessing(tester, testlabels)
-   # testStuff = testStuff.reshape(testStuff.shape[0], 28, 28, 1)
-    #y_pred = model.predict(testStuff).round()
-    #print(accuracy_score(testlabels, y_pred))
+        testStuff, testlabels = preProcessing(tester, testlabels)
+        testStuff = testStuff.reshape(testStuff.shape[0], 28, 28, 1)
+        y_pred = model.predict(testStuff).round()
+        print(accuracy_score(testlabels, y_pred))
     #score = accuracy_score()
-    model.save("my_model.hl5")
+        model.save("my_model.hl5")
 
